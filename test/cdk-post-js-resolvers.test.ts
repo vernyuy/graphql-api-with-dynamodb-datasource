093a9d0a1d4596f3a05811542cf21759f19b76c7
vernyuy <fonchu.e.venyuy@gmail.com>
2023-08-20 15:29:40 +0100

    Initial commit

---
+++ test/cdk-post-js-resolvers.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPostJsResolvers from '../lib/cdk-post-js-resolvers-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-post-js-resolvers-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPostJsResolvers.CdkPostJsResolversStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
